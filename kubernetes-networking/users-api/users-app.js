28822975067a73cec1335aa1cca6646b9b46ae5d
ziemianek <mziemianek3@gmail.com>
2023-03-10 18:30:35 +0100

    Add some stuff

--- kubernetes-networking/users-api/users-app.js
+++ kubernetes-networking/users-api/users-app.js
@@ -54,7 +54,7 @@ app.post('/login', async (req, res) => {
   // normally, we'd find a user by email and grab his/ her ID and hashed password
   const hashedPassword = password + '_hash';
   const response = await axios.get(
-    `http://${process.env.AUTH_ADDRESS}/token/` + hashedPassword + '/' + password
+    `http://${process.env.AUTH_SERVICE_SERVICE_HOST}/token/` + hashedPassword + '/' + password
   );
   if (response.status === 200) {
     return res.status(200).json({ token: response.data.token });
